8d8b6dde2e4eb65493b4b08d3d5d8022be969365
Mubariz Nacafov <mubariz.necefov.17@gmail.com>
2024-10-23 00:38:06 +0400

    adding api server to project

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -8,8 +8,25 @@ import rain_icon from "../assets/rain.png";
 import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
 import cloud_icon from "../assets/cloud.png";
+import { useEffect } from "react";
 
 const Weather = () => {
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      console.log(import.meta.env.VITE_APP_ID);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    search("London");
+  }, []);
+
   return (
     <div className="weather">
       <div className="search_bar">
